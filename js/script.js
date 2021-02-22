e51f3354afdecfe8eb127b85b5cd65cdc147cde6
nikhas <nikhamalancharuvil@gmail.com>
2021-02-22 21:59:23 +0530

    Student website

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function okbtn(){
+   var n1="Details entered successfully";
+   document.getElementById("demo").innerHTML =n1;
+}
+function rebtn()
+{
+var n1="Edit your details";
+document.getElementById("demo").innerHTML =n1;
+}
